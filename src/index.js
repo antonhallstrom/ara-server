2e21a84562cc07ced7ca4cab26da5fa1d89d0370
Anton Hallstrom <antonhallstrom@live.se>
2019-01-22 16:29:57 +0100

    cors

--- src/index.js
+++ src/index.js
@@ -19,14 +19,14 @@ app.use(
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader(
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   )
 
   if (req.method === 'OPTIONS') {
-    res.setHeader(
+    res.header(
       'Access-Control-Allow-Methods',
       'PUT',
       'POST',
